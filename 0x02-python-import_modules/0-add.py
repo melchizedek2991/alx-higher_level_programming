407d7e2a4a0cceae311dc2c87dc9b6954a68684b
Ayodele Ayomidotun <ayodeleayomidotun@gmail.com>
2023-10-05 06:53:29 -0700

    All

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     a = 1
     b = 2
 
-    add(a,b)
+    solution = add(a,b)
 
-    print("{} + {} = {}".format(a, b, add(a,b)))
+    print("{} + {} = {}".format(a, b, solution))
