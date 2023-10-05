9f32fc494f36ba76489262b3e37d65fa1b7ff370
Ayodele Ayomidotun <ayodeleayomidotun@gmail.com>
2023-10-05 06:14:09 -0700

    All Task Ammended

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+
+    import sys
+
+    argc = len(sys.argv) -1
+    if argc == 0;
+        print("0")
+    elif argc == 1;
+        print("1")
+    else ("{}".format(argc))
+    for index in range(argc):
+        print("{}: {}".format(index + 1, sys.argv[index + 1]))
