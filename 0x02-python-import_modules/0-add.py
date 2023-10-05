d7b08ce0cc7a34ca88cbb4d22fbd599cd10fd013
Ayodele Ayomidotun <ayodeleayomidotun@gmail.com>
2023-10-05 03:18:20 -0700

    Task 1 myself

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from 0-add import add
+
+    a = 1
+    b = 2
+
+    solution = add(1,2)
+
+    print("{:d} + {:d} = {:d}".format(a, b, solution))
