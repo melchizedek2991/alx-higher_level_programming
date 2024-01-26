f0abfbffd6a53123c62b4ae96c62f3453f454760
Ayodele Ayomidotun <ayodeleayomidotun@gmail.com>
2024-01-26 08:34:37 -0800

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Displays the value of X-Request-Id and 
+"""Displays the X-Request-Id header variable of a request to a given URL.
 
-sends a request to the specified URL.
+Usage: ./1-hbtn_header.py <URL>
 """
 import sys
 import urllib.request
